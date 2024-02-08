77872d2ad730583f1a16a4b17891d95b45c90d97
AMBANI DAVID <ambanibulemi@gmail.com>
2024-02-08 10:10:23 +0300

    Deploy

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 with open('best_model.pkl', 'rb') as f:
     model = pickle.load(f)
-    
+
 # Define a route for the home page
 @app.route('/')
 def home():
@@ -21,7 +21,8 @@ def predict():
     input_features =[float(request.form.get('Electricity from wind - TWh')),
                   float(request.form.get('Electricity from hydro - TWh')),
                   float(request.form.get('Electricity from solar - TWh')),
-                  float(request.form.get('Other renewables including bioenergy - TWh'))
+                  float(request.form.get('Other renewables including bioenergy - TWh')),
+                  float(request.form.get('Year'))
     ]
 
     # Make prediction
